1e396cb1002d7e4da9182bddbf051ef3aa18ca93
khanh97mh <khanh97mh@gmail.com>
2018-06-18 01:30:14 +0700

    Update 18/6

---
+++ Demo-Spring-Tool-Suite/src/main/java/kientapcongnghiep/DemoSpringToolSuite/DemoSpringToolSuiteApplication.java
@@ -0,0 +1,12 @@
+package kientapcongnghiep.DemoSpringToolSuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringToolSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringToolSuiteApplication.class, args);
+	}
+}
---
+++ Demo-Spring-Tool-Suite/src/test/java/kientapcongnghiep/DemoSpringToolSuite/DemoSpringToolSuiteApplicationTests.java
@@ -0,0 +1,16 @@
+package kientapcongnghiep.DemoSpringToolSuite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringToolSuiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
