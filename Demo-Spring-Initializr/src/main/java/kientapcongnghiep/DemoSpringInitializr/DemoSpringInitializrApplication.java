7c7983f7d5d063edc313089509cb19482556572e
khanh97mh <37797667+khanh97mh@users.noreply.github.com>
2018-06-20 14:33:36 +0700

    Update 20/6

--- Demo-Spring-Initializr/src/main/java/kientapcongnghiep/DemoSpringInitializr/DemoSpringInitializrApplication.java
+++ Demo-Spring-Initializr/src/main/java/kientapcongnghiep/DemoSpringInitializr/DemoSpringInitializrApplication.java
@@ -1,9 +1,14 @@
 package kientapcongnghiep.DemoSpringInitializr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration 
+@EnableAutoConfiguration 
+@Import ({Controller.class})
 public class DemoSpringInitializrApplication {
 
 	public static void main(String[] args) {
